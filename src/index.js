68aad1511e6ef052bd70ea4d86e0090dcb135f41
Sergejs-Rudko <sergejs.rudko@gmail.com>
2023-01-03 23:26:28 +0000

    feat: reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const checker = (number) => {
+        let myArray = number.toString().split('');
+        if (myArray[myArray.length - 1] === '0') {
+            myArray.pop()
+        }
+        return +myArray.reverse().join('');
+    }
+    return checker(Math.abs(n));
 }
